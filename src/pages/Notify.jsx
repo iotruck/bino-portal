7549e8b4bfea2397dbd62c3a73956fbe6baba239
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2021-11-03 21:46:45 -0300

    card com mensagens

--- src/pages/Notify.jsx
+++ src/pages/Notify.jsx
@@ -1,62 +1,54 @@
 import React from 'react';
 import User from '../components/UserData';
 import NotifyNotification from '../components/NotifyNotification';
+import ChatBox from '../components/Chat';
 
 
 export default function Notify(props) {
     return (
         <React.Fragment>
-            
+
             <h1 className="welcome">
                 <p>
                     Notificações
-               </p>
+                </p>
             </h1>
             <div className="background">
                 <div className="left title">
                     <div>
-                    <h1>
-                        <p>
-                            Últimas <span>05 horas</span>
-                        </p>
-                    </h1>
+                        <h1>
+                            <p>
+                                Viagens <span>ativas</span>
+                            </p>
+                        </h1>
                     </div>
                     <div className="notify-screen">
-                    <NotifyNotification code="GF4657688" date="18:30" message="Última atualização há 4 horas"/>
-                    <NotifyNotification code="FO76453P2" date="11:30" message="Chat não respondido (20 min)"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
+                        <NotifyNotification code="GF4657688" date="18:30" message="Última atualização há 4 horas" />
                     </div>
                 </div>
+
                 <hr className="line-dotted" />
+
 
                 <div className="right title">
                     <div>
-                    <h1>
-                        <p>
-                            Últimas <span>15 horas</span>
-                        </p>
-                    </h1>
+                        <h1>
+                            <p>
+                                Últimas <span>15 horas</span>
+                            </p>
+                        </h1>
                     </div>
-                
-                    <div className="notify-screen">
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    <NotifyNotification code="HKLMIHEY" date="01:30" message="Última atualização há 6 horas"/>
-                    <NotifyNotification code="886652431" date="20:30" message="Movimentação não registrada"/>
-                    </div>
+
+
+                    <ChatBox 
+                        senderName="Sandra Cunha" 
+                        message="Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Sunt minima ad accusantium quia ducimus est error perferendis, 
+                        numquam perspiciatis praesentium modi fugiat molestias."
+                        dateTime = "22:43"
+                        />
+
+
                 </div>
             </div>
         </React.Fragment>
